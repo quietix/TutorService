c84212c8692725614fde8002b1e1694f5f0cb452
Ivan <i.zashchyk@gmail.com>
2023-05-21 11:24:15 +0300

    task3 is done

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,16 @@
 //
 // console.log(testModules.hello);
 
-import { showTask1 } from "./task1/task1.js";
+import {showTask1, task1} from "./task1/task1.js";
 import {showTask2} from "./task2/task2.js";
+import {prettyUsersPrint} from "./task1/functions.js";
+import {showTask3} from "./task3/task3.js";
 
 /* __Run task1:__ */
 // showTask1()
 
 /* __Run task2:__ */
 // showTask2()
+
+/* __Run task3:__ */
+// showTask3()
---
+++ src/js/task3/task3.js
@@ -0,0 +1,32 @@
+import {randomUserMock} from "../FE4U-Lab3-mock.js";
+import {task1} from "../task1/task1.js";
+import {prettyUsersPrint} from "../task1/functions.js";
+
+// filters: [country: '<country>', age: [<ageMin>, <ageMax>], gender: '<male> or <female>', favorite: <true> or <false>]
+export function task3(usersArr, filters){
+    return usersArr.filter(function (item){
+        for (let key in filters){
+            if (key === 'age'){
+                if (item[key] === undefined || filters[key][0] > item[key] || filters[key][1] < item[key]){
+                    return false
+                }
+            }
+            else if (key === 'favorite'){
+                if (item[key] === undefined || item[key] !== filters[key]){
+                    return false
+                }
+            }
+            else if (item[key] === undefined || item[key].toLowerCase() !== filters[key].toLowerCase()){
+                return false
+            }
+        }
+        return true
+    })
+}
+
+// Show task3 work
+export function showTask3(){
+    let users = task1()
+    let filteredUsers = task3(users, { country: 'Germany', age: [50, 100], gender: 'Female', favorite: false})
+    prettyUsersPrint(filteredUsers)
+}
